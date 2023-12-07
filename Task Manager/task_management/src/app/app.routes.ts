c088b0047ce35a1984a5ea675cef4afb9a411865
Muhammad Hafeezullah <146739943+PyGularCoder@users.noreply.github.com>
2023-12-07 07:07:02 +0000

    deleted

--- Task Manager/task_management/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskViewComponent } from './components/task-view/task-view.component';
-
-export const routes: Routes = [
-    {
-        path:'',
-        component:TaskViewComponent
-    }
-];
